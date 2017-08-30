c1223eb33eda38a44a0ad6c173c612a2e5354213
likunpeng <likunpeng@huaxiafinance.com>
2017-08-30 17:17:57 +0800

    init CGContextRef productor

---
+++ ReviewCGContextRef/ReviewCGContextRef/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReviewCGContextRef
+//
+//  Created by lkp on 2017/8/30.
+//  Copyright © 2017年 lkp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
