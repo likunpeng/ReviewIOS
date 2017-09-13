c2e629757c87d55182e157f42424b8aaf25bcb78
likunpeng <likunpeng@huaxiafinance.com>
2017-09-13 16:24:58 +0800

    del 第二章简单代码

--- ReviewProtocal/ReviewProtocal/main.m
+++ ReviewProtocal/ReviewProtocal/main.m
@@ -12,9 +12,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
 //        NSLog(@"Hello, World!");
-        Swallow *swallow = [[Swallow alloc] init];
-        [swallow fly];
-        [swallow run];
+//        Swallow *swallow = [[Swallow alloc] init];
+//        [swallow fly];
+//        [swallow run];
+        double d2             = 8.32222222222;
+        NSString *d2Str       = [NSString stringWithFormat:@"%lf", d2];
+        NSLog(@"%@",d2Str);
+        NSDecimalNumber *num1 = [NSDecimalNumber decimalNumberWithString:d2Str];
+        NSString *strD2       = [num1 stringValue];
+        NSLog(@"d2: %@", strD2);
     }
     return 0;
 }
