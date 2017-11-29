e036d1febdcf283bd4e165794da051ab7a58ec11
likunpeng <likunpeng@huaxiafinance.com>
2017-11-29 11:39:27 +0800

    test

---
+++ ReviewGCD/ReviewGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReviewGCD
+//
+//  Created by lkp on 2017/10/18.
+//  Copyright © 2017年 lkp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
