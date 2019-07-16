8f2425cfc2edb4232d0f864462887c3068c72133
likunpeng <likunpeng@huaxiafinance.com>
2019-07-16 11:43:53 +0800

    GCD 复习完毕

--- runLoop/runLoop/Study/StudyGCD.m
+++ runLoop/runLoop/Study/StudyGCD.m
@@ -8,7 +8,15 @@
 
 #import "StudyGCD.h"
 
+@interface StudyGCD()
+
+@property (nonatomic, assign) NSInteger ticketSurplusCount;
+
+@end
+
 @implementation StudyGCD
+
+dispatch_semaphore_t semaphoreLock;
 
 - (void)createQueue {
     // 创建串行队列
@@ -37,7 +45,9 @@
 //    [self apply];
 //    [self groupNotify];
 //    [self groupWait];
-    [self groupEnterAndLeave];
+//    [self groupEnterAndLeave];
+//    [self semaphoreSync];
+    [self initTicketStatusSave];
 }
 
 /**
@@ -475,6 +485,84 @@
     //    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
     //
     //    NSLog(@"group---end");
+}
+
+/**
+ * semaphore 线程同步
+ */
+- (void)semaphoreSync {
+
+    NSLog(@"currentThread---%@",[NSThread currentThread]);  // 打印当前线程
+    NSLog(@"semaphore---begin");
+
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+    __block int number = 0;
+    dispatch_async(queue, ^{
+        // 追加任务1
+        [NSThread sleepForTimeInterval:2];              // 模拟耗时操作
+        NSLog(@"1---%@",[NSThread currentThread]);      // 打印当前线程
+
+        number = 100;
+
+        dispatch_semaphore_signal(semaphore);
+    });
+
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    NSLog(@"semaphore---end,number = %d",number);
+}
+
+/**
+ * 线程安全：使用 semaphore 加锁
+ * 初始化火车票数量、卖票窗口(线程安全)、并开始卖票
+ */
+- (void)initTicketStatusSave {
+    NSLog(@"currentThread---%@",[NSThread currentThread]);  // 打印当前线程
+    NSLog(@"semaphore---begin");
+
+    semaphoreLock = dispatch_semaphore_create(1);
+
+    self.ticketSurplusCount = 50;
+
+    // queue1 代表北京火车票售卖窗口
+    dispatch_queue_t queue1 = dispatch_queue_create("net.bujige.testQueue1", DISPATCH_QUEUE_SERIAL);
+    // queue2 代表上海火车票售卖窗口
+    dispatch_queue_t queue2 = dispatch_queue_create("net.bujige.testQueue2", DISPATCH_QUEUE_SERIAL);
+
+    __weak typeof(self) weakSelf = self;
+    dispatch_async(queue1, ^{
+        [weakSelf saleTicketSafe];
+    });
+
+    dispatch_async(queue2, ^{
+        [weakSelf saleTicketSafe];
+    });
+}
+
+/**
+ * 售卖火车票(线程安全)
+ */
+- (void)saleTicketSafe {
+    while (1) {
+        // 相当于加锁
+        dispatch_semaphore_wait(semaphoreLock, DISPATCH_TIME_FOREVER);
+
+        if (self.ticketSurplusCount > 0) {  //如果还有票，继续售卖
+            self.ticketSurplusCount--;
+            NSLog(@"%@", [NSString stringWithFormat:@"剩余票数：%ld 窗口：%@", (long)self.ticketSurplusCount, [NSThread currentThread]]);
+            [NSThread sleepForTimeInterval:0.2];
+        } else { //如果已卖完，关闭售票窗口
+            NSLog(@"所有火车票均已售完");
+
+            // 相当于解锁
+            dispatch_semaphore_signal(semaphoreLock);
+            break;
+        }
+
+        // 相当于解锁
+        dispatch_semaphore_signal(semaphoreLock);
+    }
 }
 
 @end
