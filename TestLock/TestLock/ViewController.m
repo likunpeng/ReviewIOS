ed59d9ae068c82b6d91378b966fe2c2d05a06235
李坤鹏 <likunpeng@sheyuan.com>
2020-09-11 10:03:27 +0800

    使用pthread_mutex实现锁 和 相关递归锁的机制

--- TestLock/TestLock/ViewController.m
+++ TestLock/TestLock/ViewController.m
@@ -19,12 +19,54 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 //    [self testSemaphore];
+//    [self testPthreadMutex];
+    [self testRecursiveLock];
+    
+}
+/**
+ 实现递归锁
+ */
+- (void)testRecursiveLock {
+    static pthread_mutex_t pLock;
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr); //初始化attr并且给它赋予默认
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE); //设置锁类型，这边是设置为递归锁
+    pthread_mutex_init(&pLock, &attr);
+    pthread_mutexattr_destroy(&attr);//销毁一个属性对象，在重新进行初始化之前该结构不能重新使用
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        static void (^RecuisiveBlock)(int);
+        RecuisiveBlock = ^(int value) {
+            pthread_mutex_lock(&pLock);
+            if (value > 0) {
+                NSLog(@"value = %d", value);
+                RecuisiveBlock(value - 1);
+            };
+            pthread_mutex_unlock(&pLock);
+        };
+        RecuisiveBlock(5);
+    });
     
 }
 
 - (void)testPthreadMutex {
     static pthread_mutex_t plock;
+    pthread_mutex_init(&plock, NULL);
+    // thread 1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"enter thread 01");
+        pthread_mutex_lock(&plock);
+        sleep(3);
+        NSLog(@"thread 01");
+        pthread_mutex_unlock(&plock);
+    });
     
+    // thread 2
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"enter thread 02");
+        pthread_mutex_lock(&plock);
+        NSLog(@"thread 02");
+        pthread_mutex_unlock(&plock);
+    });
 }
 
 - (void)testSemaphore {
